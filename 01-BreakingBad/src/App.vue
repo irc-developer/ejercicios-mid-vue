bd37b8d947fb0f7fb5d38cc6917b94c73b5b8021
menme <ivan.rodriguez.carrion@gmail.com>
2022-11-19 08:10:10 +0100

    first commit

---
+++ 01-BreakingBad/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>hola mundo</h1>
+    <p>IVAN</p>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    display:flex;
+    flex-direction: column;
+  }
+ 
+</style>
