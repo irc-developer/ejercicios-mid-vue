ac24565288270662d2fb5d56ebb9ffb8815928f5
menme <ivan.rodriguez.carrion@gmail.com>
2022-12-13 17:22:36 +0100

    Final 8

--- 02-PiniaApp/src/clients/composables/useClient.ts
+++ 02-PiniaApp/src/clients/composables/useClient.ts
@@ -1,35 +1,64 @@
-import { ref, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 import type { Client } from "@/clients/interfaces/client"
-import { useQuery } from '@tanstack/vue-query';
+import { useMutation, useQuery } from '@tanstack/vue-query';
 import clientsApi from '@/api/client-api';
+import clientApi from '@/api/client-api';
 
 const getClient = async( id:number ):Promise<Client> => {
     const { data } = await clientsApi.get(`/clients/${ id }`);
     return data;
 }
 
-const useClient = ( id:number ) => {
 
+const updateClient = async( client:Client ):Promise<Client> => {
+
+    //? El queryClientnos sirve para poder forzar el refecheo de las páginas haciendo uso del VueQueryPlugin que tenemos en el main.ts
+    //con esto detenemos el procesamemiento de la respuesta
+       // await new Promise( resolve => {
+    //     setTimeout( () => resolve(true), 2000 )
+    // });
+//     const { data } = await clientsApi.patch<Client>(`/clients/${ client.id }`, client );
+    // const queries = queryClient.getQueryCache().findAll(['clients?page='],{ exact: false });
+    // queries.forEach( query => query.reset() )
+    // queries.forEach( query => query.fetch() );
+
+
+    const { data } = await clientApi.patch<Client>(`/clients/${ client.id }`, client )
+    return data;
+}
+
+
+
+const useClient = ( id:number ) => {
+    
+    //de clientMutation también podemos extraer el isLoading pero ahora mismo hay un problema y es que se considera una propiedad reactiva con lo que tendremos que usar el .value para compensarlo
+    const clientMutation = useMutation( updateClient );
     const client = ref<Client>();
 
-    const {isLoading, data } = useQuery(
+    const {isLoading, data, isError } = useQuery(
         [ 'client', id ],
-        () => getClient(id)
+        () => getClient(id), {
+            retry:false //con esto pvevenimos que vuelva a intentar el fetch si falla.
+        }
     );
 
-    //Es importante tener claro que ahora mismo, con la asignación client.value = data.value se considera como no mutable y no nos dejaría cambiar los valores en la vista, para prevenirlo tenemos que desestructurar el valor para poder crear un nuevo puntero o directamente obviar el watch y usar el onSuccess del tanstack pero esto provocaría que perdiesemos la reactividad.
+    //Es importante tener claro que ahora mismo, con la asignación client.value = data.value estamos pasándole por referencia el valor con lo que se considera como no mutable y no nos dejaría cambiar los valores en la vista, para prevenirlo tenemos que desestructurar el data para poder crear un nuevo puntero o directamente obviar el watch y usar el onSuccess del tanstack pero esto provocaría que perdiesemos la reactividad.
     watch(data, () => {
         if ( data.value ) client.value = { ...data.value };
     }, { immediate:true } );
 
     return {
         //properties 
+        clientMutation,
+        isError,
         isLoading, 
-        client
+        client,
         //methods
-        
+        updateClient:       clientMutation.mutate,
+        isUpdating:         computed( () => clientMutation.isLoading.value ),
+        isUpdatingSuccess:  computed( () => clientMutation.isSuccess.value ),
+        isErrorUpdating:    computed( () => clientMutation.isError.value )
         //actions
-
     }
 }
 
--- 02-PiniaApp/src/clients/pages/ClientPage.vue
+++ 02-PiniaApp/src/clients/pages/ClientPage.vue
@@ -1,38 +1,41 @@
 <script setup lang="ts">
 import LoadingModal from '@/shared/icons/components/LoadingModal.vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import useClient from '@/clients/composables/useClient';
-import { useMutation } from '@tanstack/vue-query';
-import type { Client } from '@/clients/interfaces/client';
-import clientApi from '@/api/client-api';
+import { watch } from 'vue';
 
 const route = useRoute();
+const router = useRouter(); 
 //permite devolver un número o un NaN, que también lo es.
-const { client, isLoading } = useClient( +route.params.id );
+const { client, isLoading, isError, clientMutation, updateClient, isUpdating,
+isErrorUpdating,isUpdatingSuccess } = useClient( +route.params.id );
 
-const updateClient = async( client:Client ):Promise<Client> => {
-    const { data } = await clientApi.patch<Client>(`/clients/${ client.id }`, client )
-    return data;
-}
 
-const clientMutation = useMutation( updateClient );
+watch( isError, () => {
+    if (isError.value ) router.replace('/clients')
+})
+
+watch( clientMutation.isSuccess , () => {
+    setTimeout ( () => clientMutation.reset(), 2000  )
+})
+
 
 </script>
 
 <template>
     <div>
-        <h3>Guardando...</h3>
-        <h3>Guardado</h3>
+        <h3 v-if="isUpdating">Guardando...</h3>
+        <h3 v-if="isUpdatingSuccess">Guardado</h3>
 
         <LoadingModal v-if="isLoading" />
 
         <div v-if="client">
-            <form @submit.prevent="clientMutation.mutate( client! )">
+            <form @submit.prevent="updateClient( client! )">
                 <h1> {{ client.name }}</h1>
                 <input type="text" placeholder="Nombre" v-model="client.name"/>
                 <input type="text" placeholder="Direccion"  v-model="client.address"/>
                 <br>
-                <button type="submit">Enviar</button>
+                <button :disabled="isUpdating" type="submit">Enviar</button>
             </form>
             <code> {{ client }}</code>
         </div>
